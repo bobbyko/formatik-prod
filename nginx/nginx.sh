744946576007975e275b74781a6b948757b340c6
Bobby Kotzev <bobby.ko@gmail.com>
2017-12-12 16:52:37 -1000

    updated product scripts

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -2,6 +2,7 @@ docker service create \
     --network formatik_net \
     --replicas 1 \
     --mount type=bind,source=/root/formatik/formatik-prod/nginx/sites-enabled,destination=/etc/nginx/sites-enabled \
+    --mount type=bind,source=/root/formatik/formatik-prod/nginx/cert,destination=/etc/nginx/cert \
     --mount type=bind,source=/root/formatik/formatik-prod/nginx/nginx.conf,destination=/etc/nginx/nginx.conf \
     --publish 80:80 \
     --publish 443:443 \
@@ -14,6 +15,7 @@ docker run \
     --rm \
     -ti \
     -v ~/formatik/formatik-prod/nginx/sites-enabled:/etc/nginx/sites-enabled:ro \
+    -v ~/formatik/formatik-prod/nginx/cert:/etc/nginx/cert:ro \
     -v ~/formatik/formatik-prod/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
     -p 80:80 \
     -p 443:443 \
